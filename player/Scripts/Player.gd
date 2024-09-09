6a3f6df046ef4cf7542f50151161c2023c5854c5
carlosruan12307 <carlosruanaraujo789@gmail.com>
2024-09-09 09:06:04 -0300

    jump personagem

--- player/Scripts/Player.gd
+++ player/Scripts/Player.gd
@@ -4,19 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const jump_duration = 0.5
-
 var is_jumping = false
 var is_walking = false
-
-
 @onready var timer = $Sprite2D/Timer
-
 func _ready() -> void:
 
 		scale = Vector2(3,3)
 
 func _process(delta: float) -> void:
-	
+	print(animation.animation)
 	if animation.animation == "run" and velocity == Vector2.ZERO:
 		animation.play("idle")
 	if animation.animation == "jump" and is_on_floor() and is_jumping:
@@ -25,7 +21,7 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 		# Handle jump.
-	
+
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		animation.play("jump")
 		await get_tree().create_timer(.5).timeout
@@ -41,7 +37,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		
+
 		velocity.x = direction * SPEED
 		if Input.is_action_pressed("ui_right"):
 			is_walking = true
