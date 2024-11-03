6ab7a9f7639fa26268afab8f24a0abf9e91e61d2
carlosruan12307 <carlosruanaraujo789@hotmail.com>
2024-11-03 10:01:54 -0300

    final

--- game/Scripts/AreaForaDoJogo.gd
+++ game/Scripts/AreaForaDoJogo.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 
 func goToNextArea(body: Node2D) -> void:
 	
-	if PlayerStatus.thisBodyIsPlayer(body) and progressBarLife.value >= 1000: handleNextPhase(PhasesStates.currentPhase)
+	if PlayerStatus.thisBodyIsPlayer(body) and progressBarLife and  progressBarLife.value >= 1000: handleNextPhase(PhasesStates.currentPhase)
 	
 
 func handleNextPhase(currentPhase) -> void:
--- player/atack_bow.gd
+++ player/atack_bow.gd
@@ -17,6 +17,7 @@ func Enter():
 	is_attacking = true
 	player.velocity = Vector2.ZERO  # Para o movimento do jogador
 	original_scale = spritePlayer.scale  # Armazena a escala original
+	#update_player_scale()
 	rotate_player_towards_enemy()  # Rotaciona o jogador em direção ao inimigo
 	animation.play("teste")  # Inicia a animação de ataque
 
@@ -41,17 +42,30 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
-
+func update_player_scale():
+	# Verifica se o inimigo está à esquerda ou à direita do jogador
+	if enemy.global_position.y < player.global_position.y:
+		spritePlayer.scale.y = -1  # Inverte o eixo X se o inimigo estiver à esquerda
+	else:
+		spritePlayer.scale.y = 1  # Mantém a escala normal se o inimigo estiver à direita
 func rotate_player_towards_enemy():
 	var direction = (enemy.global_position - spritePlayer.global_position).normalized()
-	spritePlayer.rotation = direction.angle()  # Rotaciona o jogador para olhar em direção ao inimigo
+	 # Rotaciona o jogador para olhar em direção ao inimigo
 
 	# Define a escala com base na direção
-	if direction.x < 0:
-		spritePlayer.scale.x = -1  # Inverte a escala no eixo X se o inimigo estiver à esquerda
-	else:
-		spritePlayer.scale.x = 1  # Mantém a escala normal se o inimigo estiver à direita
-
+	#if direction.x < 0:
+		#spritePlayer.scale.x = -1  # Inverte a escala no eixo X se o inimigo estiver à esquerda
+	#else:
+		#spritePlayer.scale.x = 1  # Mantém a escala normal se o inimigo estiver à direita
+	if spritePlayer.scale.x >= 0 and direction.x <= 0:
+		spritePlayer.scale.y = -1
+	elif spritePlayer.scale.x <= 0 and direction.x <= 0:
+		
+		direction = direction * -1
+	elif spritePlayer.scale.x <= 0 and  direction.x >= 0:
+		direction = direction * -1
+		spritePlayer.scale.y = -1
+	spritePlayer.rotation = direction.angle() 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animation.animation == "teste":
 		spritePlayer.rotation = 0  # Reseta a rotação após o ataque
