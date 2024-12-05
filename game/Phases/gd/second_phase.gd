75c3f485c72bdc91e5f81239190f8f2635efb1fa
tarcizo12 <tarcizon12@gmail.com>
2024-12-04 22:29:13 -0300

    feat: Implementa logica de timer para a pergunta

--- game/Phases/gd/second_phase.gd
+++ game/Phases/gd/second_phase.gd
@@ -7,16 +7,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bow.position = marker.position
-	
-			# Instanciar a cena
-	#var instance = question.instantiate()
-	#
-	## Posicionar a cena no topo da tela
-	## Aqui consideramos a posição (x, y), onde x é a posição horizontal e y é a altura do topo
-	#instance.position = Vector2(get_viewport().size.x / 2, 0)  # Centraliza horizontalmente no topo
-
-	# Adiciona a instância como um filho do nó atual
-	#add_child(instance)
 	pass # Replace with function body.
 
 
--- game/Questions/question.gd
+++ game/Questions/question.gd
@@ -14,19 +14,59 @@ var oneTimeFirstPhase = true
 @onready var BotaoFalso  = $Container/GridContainer/DungeonBrickWallGrey_png/Falso
 @onready var BotaoVerdadeiro = $Container/GridContainer/DungeonBrickWallGrey_png/Verdadeiro
 @onready var COR_ORIGINAL_BOTAO = BotaoVerdadeiro.modulate 
-
-	
+@onready var labelTimer = $TimerLabel
+@onready var clock_timer = $Timer
 var perguntaAtual: PoolDePerguntas.QuestionClass
+
 
 func _ready() -> void:
 	perguntaAtual = PoolDePerguntas.getPerguntaRandomizada()
 	print("Resposta correta: ", perguntaAtual.is_pergunta_correta())
 	setPerguntaLabel(perguntaAtual.get_pergunta())
+	_clock_start()
 	pass 
+	
+	# Variável para armazenar o tempo decorrido
+var tempo_decorrido: float = 0.0
+var tempo_limite: float = 20.00
+var epsilon: float = 0.01  # Tolerância para a comparação
+
+# Atualiza o tempo decorrido e exibe no label
+func _process(delta: float) -> void:
+	tempo_decorrido += delta
+	
+	# Verifica se a diferença entre os valores é menor que a tolerância
+	if abs(tempo_decorrido - tempo_limite) < epsilon:
+		hideGetCollorButton(false)
+		print(tempo_limite == tempo_decorrido , "sao iguais")
+		executarAcaoRespostaErrada()
+		
+	_update_timer_label()
+
+# Função para iniciar o timer
+func _clock_start() -> void:
+	tempo_decorrido = 0.0 # Reseta o tempo ao iniciar
+	clock_timer.start()
+
+func _clock_stop_and_capture() -> float:
+	clock_timer.stop()
+	return tempo_decorrido
+	
+func calcular_score() -> int:
+	# Define os valores de normalização
+	var pontuacao_maxima: float = 500.0
+	var pontuacao_minima: float = 1.0
+	
+	# Fórmula para calcular a pontuação baseada no tempo (quanto menor o tempo, maior o score)
+	var pontuacao: float = pontuacao_maxima / (1 + _clock_stop_and_capture())
+
+	return max(int(round(pontuacao)), pontuacao_minima)
+
+func _update_timer_label() -> void:
+	labelTimer.text = "Tempo: " + str(tempo_decorrido).pad_decimals(2) + "s" 
 
 func setPerguntaLabel(texto: String) -> void:
 	LabelPergunta.text = texto
-
 	
 func handleButtonRespostaPressed(is_active: bool, nomeBotaoPressionado: String):
 	var respondeuCorretamente: bool = perguntaAtual.is_pergunta_correta() == is_active
@@ -41,27 +81,29 @@ func handleButtonRespostaPressed(is_active: bool, nomeBotaoPressionado: String):
 
 	await get_tree().create_timer(3.0).timeout
 	botaoPressionado.modulate = COR_ORIGINAL_BOTAO
-
-	if respondeuCorretamente:
-		executarAcaoRespostaCorreta()
-	else:
-		executarAcaoRespostaErrada()
-
 	
+	print("Score gerado: ", calcular_score())
+	
+	var novaQuestaoGerada: PerguntasUtil.PerguntaRespondida = PerguntasUtil.PerguntaRespondida.new(
+		0, respondeuCorretamente, 1.0
+	)
+
+	#if respondeuCorretamente:
+		#executarAcaoRespostaCorreta()
+	#else:
+		#executarAcaoRespostaErrada()
+
 func hideGetCollorButton(reslt: bool):
 	const cor_verde = Color(0, 1, 0) 
 	const cor_vermelha = Color(1, 0, 0)  	
-	return cor_verde if reslt else cor_vermelha
-	
-
+	return cor_verde if reslt else cor_vermelha	
 
 func hideBottaoPressionado(nomeBotaoAtual: String):
 	print("Nome do botao pressionado: ", nomeBotaoAtual)
 	match nomeBotaoAtual:
 		"BotaoVerdadeiro": return BotaoVerdadeiro
 		"BotaoFalso": return BotaoFalso
-	
-	
+		
 func executarAcaoRespostaCorreta() -> void:
 	if phase != null:
 		match PhasesStates.currentPhase:
